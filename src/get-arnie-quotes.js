701274a977df25e63fd2c3de27f9b0fc37bd2f82
Amit Sharma <amitsharma@192-168-1-101.tpgi.com.au>
2021-07-05 00:52:19 +1000

    getArnieQuotes method implementation

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,9 +1,63 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
+
+/**
+ *
+ * Resolving collection promises for promise-all action
+ * @param {*} urls[]
+ * @returns Promise collection for Promise.all([]) from httpGet<> method
+ */
+
+const getHTTPresponse = async (urls) => {
+  return await urls.map(async (url) => await httpGet(url));
+};
+
+/**
+ *
+ * @param {*} urls[]
+ * @returns Promise resolved Array of "Arnie Quotes" with key value
+ */
+
+const getPromiseResonse = async (urls) => {
+  const responseArray = [];
+  const promiseRes = await getHTTPresponse(urls);
+
+  const response = await Promise.all(promiseRes);
+  response.forEach((res) => {
+    let message = JSON.parse(res.body).message;
+
+    if (res.status === 200) {
+      responseArray.push({ "Arnie Quote": message });
+    }
+    if (res.status === 500) {
+      responseArray.push({ FAILURE: message });
+    }
+  });
+  return responseArray;
+};
+
+/**
+ * getArnieQuotes will only pass URLS and return Resolved promise
+ * Idea is single function is responsible for single task - like SOLID principle
+ * Implementing S from SOLID here
+ */
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  return await getPromiseResonse(urls);
 };
+
+/*
+code is commented for local testing - not removing just to share base approach
+const urls = [
+  "http://www.smokeballdev.com/arnie0",
+  "http://www.smokeballdev.com/arnie1",
+  "http://www.smokeballdev.com/arnie2",
+  "http://www.smokeballdev.com/arnie3",
+];
+
+urls.forEach((url) => {
+  getArnieQuotes(urls);
+});
+*/
 
 module.exports = {
   getArnieQuotes,
